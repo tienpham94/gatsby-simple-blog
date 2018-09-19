72b75e8c40d7a4ba1d081c2392b62c409fa0d31f
Tien Pham <r3dsky02@yahoo.com.vn>
2018-09-19 20:58:30 +0300

    styled-components

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,28 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import Link from 'gatsby-link'
+import styled from 'styled-components'
 
 import logo from '../images/logo.svg'
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
+const HeaderWrapper = styled.div`
+  background: #524763;
+  margin-bottom: 1.45rem;
+  h1 {
+    img {
+      height: 80px;
+    }
+  }
+`
+
+const HeaderContainer = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`
+
+const Header = ({ data }) => (
+  <HeaderWrapper>
+    <HeaderContainer>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
@@ -28,18 +34,18 @@ const Header = ({ siteTitle }) => (
           <img src={logo} alt="Level Up Logo" />
         </Link>
       </h1>
-    </div>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-    </nav>
-  </div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </HeaderContainer>
+  </HeaderWrapper>
 )
 
 export default Header
