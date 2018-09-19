ae78c429d7a7547a30db3ccdd4358f2a114ff56a
Tien Pham <r3dsky02@yahoo.com.vn>
2018-09-19 22:37:07 +0300

    Fix image component

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,31 +1,19 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
-    desc: 'A new blog',
+    title: "Gatsby Default Starter",
+    desc: "A new blog"
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-styled-components",
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-      },
+        name: "img",
+        path: `${__dirname}/src/images`
+      }
     },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'img',
-        path: `${__dirname}/src/images`,
-      },
-    },
-    'gatsby-plugin-offline',
-    'gatsby-plugin-styled-components',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-  ],
-}
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp"
+  ]
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,25 @@
-import React from 'react'
-
-import Layout from '../components/layout'
+import React from "react";
+import Link from "gatsby-link";
 
 const IndexPage = ({ data }) => (
-  <Layout>
-    <h1>Hi people</h1>
-
+  <div>
+    <h1>Hello people</h1>
+    <p>{data.site.siteMetadata.title}</p>
+    <p>{data.site.siteMetadata.desc}</p>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-  </Layout>
-)
+  </div>
+);
 
-export default IndexPage
+export default IndexPage;
 
-
+export const query = graphql`
+  query SiteMeta {
+    site {
+      siteMetadata {
+        title
+        desc
+      }
+    }
+  }
+`;
