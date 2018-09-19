699f5298987d1a5c1b347e60fcfe7dd066295f57
Tien Pham <r3dsky02@yahoo.com.vn>
2018-09-19 18:55:36 +0300

    Scaffold

---
+++ src/components/header.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+const Header = ({ siteTitle }) => (
+  <div
+    style={{
+      background: 'rebeccapurple',
+      marginBottom: '1.45rem',
+    }}
+  >
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '1.45rem 1.0875rem',
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: 'white',
+            textDecoration: 'none',
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <Link
+            to="/about"
+          >
+            About
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  </div>
+)
+
+export default Header
